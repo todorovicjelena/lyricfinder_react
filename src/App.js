9c4a328a291b5382a177b7b0d5895865b99165a6
Jelena <todorovicjelenajeka@gmail.com>
2019-09-25 19:30:35 +0200

    lyric finder

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import { Provider } from "./context";
+import Lyrics from "./components/tracks/Lyrics"
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+                <Route exact path="/lyrics/track/:id" component={Lyrics} />  
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
+    );
+  }
+}
---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Spinner from "../layout/Spinner";
+import Moment from "react-moment";
+
+class Lyrics extends Component {
+  state = {
+    track: {},
+    lyrics: {}
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_JT_KEY}`
+      )
+      .then(res => {
+        this.setState({ lyrics: res.data.message.body.lyrics }); //putanja
+
+        return axios
+          .get(
+            `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.get?track_id=${this.props.match.params.id}&apikey=${process.env.REACT_APP_JT_KEY}`
+          )
+          .then(res => {
+            this.setState({ track: res.data.message.body.track }); //putanja
+          });
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { track, lyrics } = this.state;
+    // console.log(track);
+
+    if (
+      track === undefined ||
+      lyrics === undefined ||
+      (track === Object.keys(track).length) === 0 ||
+      Object.keys(lyrics).length === 0
+    ) {
+      return <Spinner />;
+    } else {
+      return (
+        <React.Fragment>
+          <Link to="/" className="btn btn-dark btn-sm mb-4">
+            Go Back
+          </Link>
+          <div className="card">
+            <h5 className="card-header">
+              {track.track_name} by{" "}
+              <span className="text-secondary">{track.artist_name}</span>
+            </h5>
+            <div className="card-body">
+              <p className="card-text">{lyrics.lyrics_body}</p>
+            </div>
+          </div>
+          <ul className="list-group mt-3">
+            <li className="list-group-item">
+              <strong> Album ID</strong>: {track.album_id}
+            </li>
+            <li className="list-group-item">
+              <strong>Explicit Words</strong>:{" "}
+              {track.explicit === 0 ? "No" : "Yes"}
+            </li>
+            <li className="list-group-item">
+              <strong>Updated time</strong>:{" "}
+              <Moment format="MM/DD/YYYY">{track.updated_time}</Moment>
+            </li>
+          </ul>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+export default Lyrics;
