72261ee1e26106bbe2ccd12b924a3147b2019474
Jelena <todorovicjelenajeka@gmail.com>
2019-09-26 13:00:02 +0200

    app

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
-<Router>
+<Router basename={process.env.PUBLIC_URL}>
 <App />
 </Router>
 , document.getElementById('root'));
+
+
 
