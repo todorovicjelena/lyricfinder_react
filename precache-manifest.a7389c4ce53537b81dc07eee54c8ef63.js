3fe129cb65c6272e9ff372a47d3567f9ab9e826e
Jelena <todorovicjelenajeka@gmail.com>
2019-09-26 15:04:58 +0200

    Updates

---
+++ precache-manifest.a7389c4ce53537b81dc07eee54c8ef63.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2de863af9b6f3f95d71711ea42380c",
+    "url": "/lyricfinder_react/index.html"
+  },
+  {
+    "revision": "eed3cb7e9b63be3874b3",
+    "url": "/lyricfinder_react/static/js/2.972e053d.chunk.js"
+  },
+  {
+    "revision": "e50a65dbad894425a6da",
+    "url": "/lyricfinder_react/static/js/main.a8b88e1b.chunk.js"
+  },
+  {
+    "revision": "b309307ca1bac593056f",
+    "url": "/lyricfinder_react/static/js/runtime-main.82ce7365.js"
+  }
+]);
